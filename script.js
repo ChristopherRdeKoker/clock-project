ca52fbf34fa17b6ef17c2e5cb33a032f00d4f016
ChristopherRdeKoker <christopherdekoker@gmail.com>
2022-09-12 09:57:08 +0200

    working on JS func for clock

---
+++ script.js
@@ -0,0 +1,8 @@
+setInterval(setClock, 1000);
+
+function setClock() {
+  const currentDate = new Date();
+  const seconds = currentDate.getSeconds() / 60;
+  const minutes = (seconds + currentDate.getMinutes()) / 60;
+  const hours = (minutes + currentDate.getHours()) / 12;
+}
